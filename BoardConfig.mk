200b5d9ff25baed8efb1f5d678b3aca9da8ce8e2
Raphael Mounier <mounierr07@gmail.com>
2026-01-03 18:22:36 +0100

    Initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,169 @@
+DEVICE_PATH := device/huawei/marie
+ALLOW_MISSING_DEPENDENCIES := true
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := marie,kirin710
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := kirin710
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := kirin710
+TARGET_BOARD_PLATFORM_GPU := kirin
+BUILD_BROKEN_DUP_RULES := true
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_SMP := true
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+
+TARGET_USES_64_BIT_BINDER := true
+TARGET_SUPPORTS_64_BIT_APPS := true
+
+# Kernel
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_IMAGE_NAME := dummykernel
+BOARD_CUSTOM_BOOTIMG_MK := device/huawei/marie/custombootimg.mk
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x8000 --ramdisk_offset 0x01000000 --tags_offset 0x0100
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 25165824
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_CACHEIMAGE_PARTITION_SIZE := 16777216
+BOARD_FLASH_BLOCK_SIZE := 4096
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
+
+
+# System as root
+#BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+BOARD_ROOT_EXTRA_FOLDERS := d hw_odm twres splash2 sec_storage
+
+# File System
+TARGET_EXFAT_DRIVER := exfat
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+TARGET_USERIMAGES_USE_EROFS := true
+
+# Workaround for error copying vendor files to recovery ramdisk
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
+TARGET_COPY_OUT_PRODUCT := product
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
+
+# Recovery
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
+
+
+# Fstab and init.rc files
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/twrp.flags
+TARGET_RECOVERY_DEVICE_DIRS += $(DEVICE_PATH)
+
+
+# TWRP specific build flags
+TW_THEME := shrp_dark_portrait_hdpi
+BOARD_SUPPRESS_SECURE_ERASE := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_EXTRA_LANGUAGES := true
+TW_INCLUDE_NTFS_3G := true
+TW_USE_TOOLBOX := true
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd_backlight0/brightness
+TW_MAX_BRIGHTNESS := 2048
+TW_DEFAULT_BRIGHTNESS := 1200
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/config/usb_gadget/g1/functions/mass_storage.gs6/lun.%d/file"
+TW_CUSTOM_BATTERY_PATH := /sys/class/power_supply/Battery
+TW_NO_HAPTICS := true
+TARGET_USES_LOGD := true
+TW_NO_SCREEN_BLANK := true
+TARGET_USES_MKE2FS := true
+TW_INCLUDE_RESETPROP := true
+TWRP_INCLUDE_LOGCAT := true
+TW_USE_NEW_MINADBD := true
+TW_INTERNAL_STORAGE_PATH := "/data/media/0"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+RECOVERY_SDCARD_ON_DATA := true
+
+# LZMA Compression
+LZMA_COMPRESSION := -9
+LZMA_RAMDISK_TARGETS := recovery
+
+# Avb
+BOARD_AVB_ENABLE := true
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
+BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 2
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += \
+    --set_hashtree_disabled_flag \
+    --flags 2
+
+# Hack: prevent anti rollback
+PLATFORM_SECURITY_PATCH := 2099-12-31
+VENDOR_SECURITY_PATCH := 2099-12-31
+PLATFORM_VERSION := 16.1.0
+
+# Selinux
+SELINUX_IGNORE_NEVERALLOWS := true
+
+BOARD_SEPOLICY_DIRS += \
+    device/huawei/marie/sepolicy
+
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libandroidicu \
+    libcap \
+    libion \
+    libxml2
+
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libcap.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
+
+# ---------------------------------------------    
+# SHRP-specific lines
+# --------------------------------------------- 
+
+SHRP_PATH := device/huawei/marie
+SHRP_MAINTAINER := Iceows
+SHRP_DEVICE_CODE := marie
+
+# Recovery Type (for "About" section only)
+# Default (if not set): N/A
+SHRP_REC_TYPE := Treble
+
+# Device Type (for "About" section only)
+# Default (if not set): N/A
+SHRP_DEVICE_TYPE := A/B
+
+#SHRP_OFFICIAL := true
+#SHRP_EDL_MODE := 1
+SHRP_EXPRESS := true
+SHRP_EXPRESS_USE_DATA := true
+
+SHRP_INTERNAL := /sdcard
+SHRP_EXTERNAL := /external_sd
+SHRP_OTG := /usb_otg
+SHRP_FLASH := 1
+SHRP_FLASH_MAX_BRIGHTNESS := 1
+SHRP_CUSTOM_FLASHLIGHT := true
+SHRP_FONP_1 := /sys/class/leds/torch/brightness
+SHRP_REC_TYPE := normal
+SHRP_REC := /dev/block/bootdevice/by-name/erecovery_ramdisk
+SHRP_DARK := true
+
