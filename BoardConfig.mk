0a884943a03564ba8a60916a711e97947ed37327
Iceows <mounierr07@gmail.com>
2026-03-10 21:28:00 +0100

    board: change TARGET_RECOVERY_FSTAB to recovery.fstab

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,7 +69,7 @@ TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
 
 
 # Fstab and init.rc files
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/twrp.flags
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
 TARGET_RECOVERY_DEVICE_DIRS += $(DEVICE_PATH)
 
 # Crypto
